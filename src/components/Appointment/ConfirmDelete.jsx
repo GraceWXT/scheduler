ae92e627187764f0a20aa5506d52c0c35a9c920f
Grace <wangxiaotian2012@gmail.com>
2022-04-20 21:13:35 -0700

    Update the <ConfirmDelete> component to use props

--- src/components/Appointment/ConfirmDelete.jsx
+++ src/components/Appointment/ConfirmDelete.jsx
@@ -2,13 +2,17 @@ import React from "react";
 import Button from "components/Button";
 
 export default function ConfirmDelete(props) {
+
+  const { message, handleConfirm, handleCancel } = props;
+
   return (
     <main className="appointment__card appointment__card--confirm">
-      <h1 className="text--semi-bold">Delete the appointment?</h1>
+      <h1 className="text--semi-bold">{message}</h1>
       <section className="appointment__actions">
-        <Button danger>Cancel</Button>
-        <Button danger>Confirm</Button>
+        <Button danger onClick={handleConfirm}>Confirm</Button>
+        <Button danger onClick={handleCancel}>Cancel</Button>
       </section>
     </main>
   );
+
 }
--- src/components/Appointment/Show.jsx
+++ src/components/Appointment/Show.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 export default function Show(props) {
+  
   const { student, interviewer, handleEdit, handleDelete } = props;
 
   return (
