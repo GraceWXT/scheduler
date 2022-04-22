f5676c0a984b535840609fa497f24ee9dc2242d7
Grace <wangxiaotian2012@gmail.com>
2022-04-21 20:16:04 -0700

    Implement the getAppointmentsForDay selector and run unit tests for it

---
+++ src/helpers/selectors.js
@@ -0,0 +1,21 @@
+const getAppointmentsForDay = (state, selectedDay) => {
+
+  const { days, appointments } = state;
+
+  if (days.length === 0) {
+    return [];
+  }
+
+  const dayObj = days.filter(day => day.name === selectedDay)[0];
+
+  if (!dayObj) {
+    return [];
+  }
+  
+  const appointmentsIdArr = dayObj.appointments;
+  const appointmentsObjArr = appointmentsIdArr.map(id => appointments[id]);
+
+  return appointmentsObjArr;
+};
+
+export { getAppointmentsForDay };
