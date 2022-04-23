e4db2375f1035fafd4c786dbdb74f1788111fb16
Grace <wangxiaotian2012@gmail.com>
2022-04-22 20:11:55 -0700

    Update the Appointment component to use mode as the rendering condition

--- src/components/Appointment/index.jsx
+++ src/components/Appointment/index.jsx
@@ -3,12 +3,25 @@ import "components/Appointment/styles.scss";
 import Header from "./Header";
 import Show from "./Show";
 import Empty from "./Empty";
+import useVisualMode from "hooks/useVisualMode";
 
 export default function Appointment(props) {
 
   const { time, interview } = props;
+  
+  const EMPTY = "EMPTY";
+  const SHOW = "SHOW";
 
-  const body = interview ? <Show {...interview}/> : <Empty />;
+  const { mode, transition, back } = useVisualMode(interview ? SHOW : EMPTY);
+
+  const body = (() => {
+    switch (mode) {
+    case EMPTY:
+      return <Empty />;
+    case SHOW:
+      return <Show {...interview}/>;
+    }
+  })();
 
   return (
     <article className="appointment">
