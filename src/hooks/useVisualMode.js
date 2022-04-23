8519e0a74ca4e78ecf1167f7df39c07f59ed3df1
Grace <wangxiaotian2012@gmail.com>
2022-04-22 18:56:30 -0700

    Implement the back function to pass the third test

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -1,8 +1,18 @@
 import { useState } from "react";
 const useVisualMode = (initialVal) => {
   const [ mode, setMode ] = useState(initialVal);
-  const transition  = (mode) => setMode(mode);
-  return { mode, transition };
+  const [history, setHistory] = useState([initialVal]);
+
+  const transition  = (mode) => {
+    setMode(mode);
+    setHistory(prev => [...prev, mode]);
+  };
+  const back = () => {
+    setMode(history[history.length - 2 ]);
+    setHistory([...history].slice(0, -1));
+  };
+
+  return { mode, transition, back };
 };
 
 export default useVisualMode;
