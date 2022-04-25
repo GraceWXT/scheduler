66df6692b44d949eab449d60e6e46f3517d0a555
Grace <wangxiaotian2012@gmail.com>
2022-04-25 09:14:24 -0700

    Update selector comments

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,11 +1,11 @@
-/** A function that given the state variables and a selected day,
+/** A function that given the state variables,
  * returns an array of Appointment objects (that are references
  * of the appointments object properties)
  */
 const getAppointmentsForDay = ({ days, appointments, selectedDay }) => {
 
   const dayObj = days.find(day => day.name === selectedDay);
-  
+
   if (days.length === 0 || !dayObj) return [];
   
   const appointmentsIdArr = dayObj.appointments;
@@ -15,7 +15,7 @@ const getAppointmentsForDay = ({ days, appointments, selectedDay }) => {
 
 };
 
-/** A function that given the state variables and a selected day,
+/** A function that given the state variables,
  * returns an array of interviewer objects
  */
 const getInterviewersForDay = ({ days, interviewers, selectedDay }) => {
@@ -31,7 +31,7 @@ const getInterviewersForDay = ({ days, interviewers, selectedDay }) => {
 
 };
 
-/** A Function that given an interview object with "interviewer: id" property,
+/** A Function that given the interviewers objects and an interview object with "interviewer: id" property,
  *  returns a copy of the interview object with the matching "interviewer: object" property */
 const getInterview = ({ interviewers }, interview) => {
   if (!interview) return null;
