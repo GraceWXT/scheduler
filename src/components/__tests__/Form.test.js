bf9d13efc31087ed9d23519645f5f131794e8d67
Grace <wangxiaotian2012@gmail.com>
2022-04-26 15:33:55 -0700

    Implement the fifth test for <Form>

--- src/components/__tests__/Form.test.js
+++ src/components/__tests__/Form.test.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { render, cleanup } from "@testing-library/react";
+import { render, cleanup, fireEvent } from "@testing-library/react";
 
 import Form from "components/Appointment/Form";
 
@@ -30,30 +30,58 @@ describe("Form", () => {
   });
 
   it("validates that the student name is not blank", () => {
+    /* 1. Create the mock handleSave function */
+
+    /* 2. Render the Form with interviewers and the handleSave mock function passed as an handleSave prop, the student prop should be blank or undefined */
+
+    /* 3. Click the save button */
+
     /* 1. validation is shown */
     expect(getByText(/student name cannot be blank/i)).toBeInTheDocument();
   
-    /* 2. onSave is not called */
-    expect(onSave).not.toHaveBeenCalled();
+    /* 2. handleSave is not called */
+    expect(handleSave).not.toHaveBeenCalled();
   });
   
   it("validates that the interviewer cannot be null", () => {
-    /* 3. validation is shown */
+    /* 1. Create the mock handleSave function */
+
+    /* 2. Render the Form with interviewers and the handleSave mock function passed as an handleSave prop, the interviewer prop should be null */
+
+    /* 3. Click the save button */
+
+    /* 1. validation is shown */
     expect(getByText(/please select an interviewer/i)).toBeInTheDocument();
   
-    /* 4. onSave is not called */
-    expect(onSave).not.toHaveBeenCalled();
+    /* 1. handleSave is not called */
+    expect(handleSave).not.toHaveBeenCalled();
   });
   
-  it("calls onSave function when the name is defined", () => {
-    /* 5. validation is not shown */
+  it("calls handleSave function when the name is defined", () => {
+    /* 1. Create the mock handleSave function */
+    const handleSave = jest.fn();
+
+    /* 2. Render the Form with interviewers, name and the handleSave mock function passed as an handleSave prop */
+    const { queryByText, getByText } = render(
+      <Form
+        student="Lydia Miller-Jones"
+        interviewer={1}
+        interviewers={interviewers}
+        handleSave={handleSave}
+      />
+    );
+    /* 3. Click the save button */
+    const saveButton = getByText("Save");
+    fireEvent.click(saveButton);
+
+    /* 1. validation is not shown */
     expect(queryByText(/student name cannot be blank/i)).toBeNull();
     expect(queryByText(/please select an interviewer/i)).toBeNull();
   
-    /* 6. onSave is called once*/
-    expect(onSave).toHaveBeenCalledTimes(1);
+    /* 1. handleSave is called once*/
+    expect(handleSave).toHaveBeenCalledTimes(1);
   
-    /* 7. onSave is called with the correct arguments */
-    expect(onSave).toHaveBeenCalledWith("Lydia Miller-Jones", 1);
+    /* 3. handleSave is called with the correct arguments */
+    expect(handleSave).toHaveBeenCalledWith("Lydia Miller-Jones", 1);
   });
 });
