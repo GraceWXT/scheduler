f9fd6e6ca949a6c25079a02b2ef68215a922eee2
Grace <wangxiaotian2012@gmail.com>
2022-04-26 22:20:36 -0700

    Add comment to <DayListItem>

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,15 +2,16 @@ import React from "react";
 import classNames from "classnames";
 import "components/Button.scss";
 
-export const Button = (props) => {
+export default function Button(props) {
   // Destrucure the props object
   const { confirm, danger, disabled, handleClick, children } = props;
+
   // Generate the class string conditionally
   const buttonClass = classNames("button", {
     "button--confirm": confirm,
     "button--danger": danger
   });
-  
+
   return (
     <button
       disabled={disabled}
@@ -21,4 +22,4 @@ export const Button = (props) => {
     </button>
   );
 
-};
+}
--- src/components/DayListItem.jsx
+++ src/components/DayListItem.jsx
@@ -3,14 +3,16 @@ import classNames from "classnames";
 import "components/DayListItem.scss";
 
 export default function DayListItem(props) {
-
+  // Destrucure the props object
   const { name, spots, selected, handleClick } = props;
 
+  // Generate the class string conditionally
   const classString = classNames("day-list__item", {
     "day-list__item--selected": selected,
     "day-list__item--full": !spots
   });
 
+  // Generate the spots remaining string conditionally
   const formatedSpots = ((spots) => {
     switch (spots) {
     case 0:
