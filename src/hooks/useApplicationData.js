d258579995f84150e39e0a46054558e910195d93
Grace <wangxiaotian2012@gmail.com>
2022-04-27 22:08:40 -0700

    Add comments for custom hooks

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -1,6 +1,7 @@
 import { useReducer, useEffect } from "react";
 import axios from "axios";
 const useApplicationData = () => {
+  // Setup the initial state
   const initialState = {
     days:[],
     selectedDay: "Monday",
@@ -8,18 +9,33 @@ const useApplicationData = () => {
     interviewers: {}
   };
 
+  // create const for each action types which works like enums
   const SET_SELECTED_DAY = "SET_DAY";
   const SET_APPLICATION_DATA = "SET_APPLICATION_DATA";
   const SET_INTERVIEW = "SET_INTERVIEW";
 
+  // A reducer function returns the updated state
+  // for different action types
   const reducer = (state, action) => {
+    // Dedestructure the action object to get the values needed
     const { selectedDay, days, appointments, interviewers, id, interview } = action;
+
     switch (action.type) {
+
+    // For action type "SET_SELECTED_DAY", update the selected day in the state object
     case SET_SELECTED_DAY:
       return { ...state, selectedDay };
+
+    // For action type "SET_APPLICATION_DATA",
+    // update the days, appointments and interviewers data in the state object
     case SET_APPLICATION_DATA:
       return { ...state, days, appointments, interviewers};
+
+    // For action type "SET_INTERVIEW",
+    // update the interview object (of given id) in the appointments object,
+    // and update the spots remaining in the days array
     case SET_INTERVIEW: {
+      // Update appointments
       const appointment = {
         ...state.appointments[id],
         interview
@@ -28,6 +44,7 @@ const useApplicationData = () => {
         ...state.appointments,
         [id]: appointment
       };
+      // Update days
       const updateSpotsOfDays = (id, appointments) => {
         const oldDayObj = state.days.find(day => day.appointments.includes(id));
         const spots = oldDayObj.appointments.filter((appoId) => !appointments[appoId].interview).length;
@@ -38,8 +55,10 @@ const useApplicationData = () => {
         return days;
       };
       const days = updateSpotsOfDays(id, appointments);
+      // Return the state with updated appointments and days
       return { ...state, appointments, days};
     }
+    // Throw an error if the action type is not listed above
     default:
       throw new Error(
         `Tried to reduce with unsupported action type: ${action.type}`
@@ -47,8 +66,12 @@ const useApplicationData = () => {
     }
   };
 
+  // use reducer to manage the state
   const [state, dispatch] = useReducer(reducer, initialState);
 
+  // Fetch the days, appointments and interviewers data
+  // and dispatch the SET_APPLICATION_DATA action to update state
+  // Happens only once when the <Application> is first rendered
   useEffect(() => {
 
     const daysPromise = axios.get("/api/days");
@@ -67,25 +90,36 @@ const useApplicationData = () => {
       
   }, []);
 
+  // Three functions to be returned and passed to other components
+  // as event handlers where set state is needed
+
+  // Set the selected day when it's changed
   const setSelectedDay = selectedDay => dispatch({ type: SET_SELECTED_DAY, selectedDay });
 
+  // Makes a request to the database API and update the state
+  // when a new or edited interview is saved
   const bookInterview = (id, interview) => {
     return axios.put(`/api/appointments/${id}`, {interview})
       .then(() => dispatch({type:SET_INTERVIEW, id, interview}));
   };
 
+  // Makes a request to the database API and update the state
+  // when an interview is deleted
   const deleteInterview = (id) => {
     return axios.delete(`/api/appointments/${id}`)
       .then(() => dispatch({type:SET_INTERVIEW, id, interview: null}));
   };
 
   useEffect(() => {
+    // Use websocket to make persistent connection with the API server
     const webSocket = new WebSocket(process.env.REACT_APP_WEBSOCKET_URL);
-
+    
     webSocket.addEventListener("open", () => {
       webSocket.send("ping");
     });
-
+    
+    // Listen for all SET_INTERVIEW actions and set state accordingly
+    // so that a user can see any other users' update without refreshing the page
     webSocket.addEventListener("message", (event) => {
       const message = JSON.parse(event.data);
       // console.log('Message Received: ', message);
@@ -99,6 +133,7 @@ const useApplicationData = () => {
       }
     });
 
+    // Return a cleanup function to close the websocket connection when the app is closed
     const cleanup = () => webSocket.close();
 
     return cleanup;
--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -1,8 +1,16 @@
 import { useState } from "react";
+
+/** A custom hook that keeps track of the visual mode (of the appointment body)
+ * and mode history, and provides functions to transition to another mode
+ * or go back to a previous mode
+ */
 const useVisualMode = (initialVal) => {
   const [ mode, setMode ] = useState(initialVal);
   const [ history, setHistory ] = useState([initialVal]);
 
+  /** A function that sets the mode and add the mode to the history.
+   * The second parameter defaults to false.
+   * If it's set to true, the new mode will replace the last mode in the history. */
   const transition  = (mode, replace = false) => {
     if (replace) {
       setHistory(prev => [...prev].slice(0, -1));
@@ -10,6 +18,9 @@ const useVisualMode = (initialVal) => {
     setMode(mode);
     setHistory(prev => [...prev, mode]);
   };
+
+  /** A function that sets the mode to the previous one in the history,
+   * and removes the current one from the histroy. */
   const back = () => {
     if (history.length <= 1) return;
     setMode(history[history.length - 2 ]);
