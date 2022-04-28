1a203c2eab37d61af65e2ec8813c6bb5e77c6e35
Grace <wangxiaotian2012@gmail.com>
2022-04-27 20:13:06 -0700

    Add a test for editing an interview

--- cypress/integration/0-app-tests/appointments.spec.js
+++ cypress/integration/0-app-tests/appointments.spec.js
@@ -7,12 +7,31 @@ describe("Appointments", () => {
   });
 
   it("should book an interview", () => {
-    
+    // Click on the Add button
     cy.get("[alt='Add']").first().click();
+
+    // Enter the student name, select the interviewer and save
     cy.get("[data-testid='student-name-input']").type("Lydia Miller-Jones");
     cy.get("[alt='Sylvia Palmer']").click();
     cy.contains("Save").click();
+
+    // Expect the booked interview to show the correct information
     cy.contains(".appointment__card--show", "Lydia Miller-Jones");
     cy.contains(".appointment__card--show", "Sylvia Palmer");
   });
+
+  it("should edit an interview", () => {
+    // Click on the Edit button
+    cy.get("[alt='Edit']").first().click({ force: true });
+
+    // Clear the input, enter the student name, change the selected interviewer and save
+    cy.get("[data-testid='student-name-input']").clear().type("Lydia Miller-Jones");
+    cy.get("[alt='Tori Malcolm']").click();
+    cy.contains("Save").click();
+    
+    // Expect the booked interview to show the correct information
+    cy.contains(".appointment__card--show", "Lydia Miller-Jones");
+    cy.contains(".appointment__card--show", "Sylvia Palmer");
+  });
+
 });
