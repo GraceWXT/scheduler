068b4d182dd0d6cb5f45ab9c11eef72f9d0fdf13
Grace <wangxiaotian2012@gmail.com>
2022-04-27 19:24:48 -0700

    Add test commands for booking an interview

--- cypress/integration/0-app-tests/appointments.spec.js
+++ cypress/integration/0-app-tests/appointments.spec.js
@@ -3,9 +3,16 @@ describe("Appointments", () => {
   beforeEach(() => {
     cy.request("/api/debug/reset");
     cy.visit("/");
+    cy.contains("Monday");
   });
 
   it("should book an interview", () => {
-    cy.contains("Monday");
+    
+    cy.get("[alt='Add']").first().click();
+    cy.get("[data-testid='student-name-input']").type("Lydia Miller-Jones");
+    cy.get("[alt='Sylvia Palmer']").click();
+    cy.contains("Save").click();
+    cy.contains(".appointment__card--show", "Lydia Miller-Jones");
+    cy.contains(".appointment__card--show", "Sylvia Palmer");
   });
 });
