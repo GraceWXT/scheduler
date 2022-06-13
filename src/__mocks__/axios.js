613f1fd12714554bf8bfed43eb536fd9fbe00de0
Grace <wangxiaotian2012@gmail.com>
2022-06-12 21:48:09 -0700

    Export a mock Axios object with the get function

--- src/__mocks__/axios.js
+++ src/__mocks__/axios.js
@@ -1,3 +1,4 @@
+// Reusable static data
 const fixtures = {
   days: [
     {
@@ -51,4 +52,31 @@ const fixtures = {
       avatar: "https://i.imgur.com/FK8V841.jpg"
     }
   }
+};
+
+// export a mock object that returns resolved Promises to match the Axios interface
+export default {
+  get: jest.fn(url => {
+    if (url === "api/days") {
+      return Promise.resolve({
+        status: 200,
+        statusText: "OK",
+        data: fixtures.days
+      });
+    }
+    if (url === "api/appointments") {
+      return Promise.resolve({
+        status: 200,
+        statusText: "OK",
+        data: fixtures.appointments
+      });
+    }
+    if (url === "api/interviewers") {
+      return Promise.resolve({
+        status: 200,
+        statusText: "OK",
+        data: fixtures.interviewers
+      });
+    }
+  })
 };
