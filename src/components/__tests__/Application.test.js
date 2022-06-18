21f52a67554f8e404c53f6c8706f257081ae1b73
Grace <wangxiaotian2012@gmail.com>
2022-06-17 17:02:48 -0700

    Add the test plan for cancelling an interview

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -62,4 +62,19 @@ describe("Application", () => {
     // Verify Monday has no spots remaining
     expect(getByText(monday, "no spots remaining")).toBeInTheDocument();
   });
+
+  it("loads data, cancels an interview and reduces the spots remaining for Monday by 1", async () => {
+    // 1. Render the Application.
+    const { container } = render(<Application />);
+
+    // 2. Wait until the text "Archie Cohen" is displayed.
+    await waitForElement(() => getByText(container, "Archie Cohen"));
+
+    // 3. Click the "Delete" button on the "Archie Cohen" appointment.
+    // 4. Check that the confirmation message is shown.
+    // 5. Click the "Confirm" button on that same appointment.
+    // 6. Check that the element with the text "Deleting" is displayed.
+    // 7. Wait until the element with the alt text "Add" is displayed.
+    // 8. Check that the DayListItem with the text "Monday" also has the text "2 spots remaining".
+  });
 });
