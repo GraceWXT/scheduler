0901ac3adf93a645c0609abf53defab68bbfbe37
Grace <wangxiaotian2012@gmail.com>
2022-06-18 23:06:13 -0700

    Add test for deleting error handling

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -194,5 +194,43 @@ describe("Application", () => {
   // Error Handling - Deleting
   it("shows the delete error when failing to delete an existing appointment", async () => {
     axios.delete.mockRejectedValueOnce();
+
+    // Render the Application.
+    const { container } = render(<Application />);
+
+    // Wait until the text "Archie Cohen" is displayed.
+    await waitForElement(() => getByText(container, "Archie Cohen"));
+
+    // Get the "Archie Cohen" appointment
+    const appointment = getAllByTestId(container, "appointment")
+      .find(appointment => queryByText(appointment, "Archie Cohen"));
+
+    // Click the "Delete" button on the "Archie Cohen" appointment.
+    fireEvent.click(getByAltText(appointment, "Delete"));
+
+    // Check that the confirmation message is shown.
+    expect(getByText(appointment, "Are you sure you would like to delete this appointment?"))
+      .toBeInTheDocument();
+
+    // Click the "Confirm" button on that same appointment.
+    fireEvent.click(getByText(appointment, "Confirm"));
+
+    // Check that the element with the text "Deleting" is displayed.
+    expect(getByText(appointment, "Deleting")).toBeInTheDocument();
+
+    // Confirm the deleting error is shown
+    await waitForElement(() => getByText(appointment, "Could not delete the appointment."));
+
+    // Click the close button
+    fireEvent.click(getByAltText(appointment, "Close"));
+
+    // Confirm the appointment card is back to SHOW
+    expect(getByText(appointment, "Archie Cohen")).toBeInTheDocument();
+
+    // Check that the DayListItem with the text "Monday" still has the text "1 spot remaining".
+    const monday = getAllByTestId(container, "day")
+      .find(day => queryByText(day, "Monday"));
+
+    expect(getByText(monday, "1 spot remaining")).toBeInTheDocument();
   });
 });
